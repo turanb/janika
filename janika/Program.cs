ffce733f36bc5416049525fb05f019343d22610d
turan.balazs@vereb.dc <turan.balazs@vereb.dc>
2019-09-20 12:12:48 +0200

    Add project files.

---
+++ janika/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mindegy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("szia hogy hivnak ?");
+            var nev = Console.ReadLine();
+            Console.WriteLine($"Oh, milyen szép név az{nev}");
+            Console.WriteLine("Szeretsz programozni?");
+            var valasz = Console.ReadLine();
+            if (valasz.ToLower() == "igen")
+            {
+                Console.WriteLine("akkor mi nagyon jo barátok leszünk !");
+            }
+            else
+            {
+                Console.WriteLine(":(");
+                Console.WriteLine("Akkor visszlát :(");
+            }
+            
+
+            Console.ReadKey();
+        }
+
+    }
+}
